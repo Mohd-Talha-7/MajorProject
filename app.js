c91ed60ad996d1fca773f44a87df96ad62f4aec0
Mohd Talha <talhalko1@gmail.com>
2025-02-15 20:08:27 +0530

    Updated app.js and added home.ejs

--- app.js
+++ app.js
@@ -125,6 +125,11 @@ app.post("/listings", async (req, res) => {
     }
 });
 
+app.get("/", (req, res) => {
+    console.log("✅ Home route accessed!");
+    res.render("home");
+});
+
 // 🌟 404 Error Handler
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
